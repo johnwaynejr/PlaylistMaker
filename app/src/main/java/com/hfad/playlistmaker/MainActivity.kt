b17b8c09546399771172cb350221f18bb6d18f8f
johnwaynejr <navuhodonosor11@gmail.com>
2022-12-16 21:43:40 +0300

    Перенос готового проекта в новый из-за системных ошибок #2

--- app/src/main/java/com/hfad/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/hfad/playlistmaker/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.hfad.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnFind = findViewById<Button>(R.id.btn_find_am)
+        btnFind.setOnClickListener {
+            val displayIntent = Intent(this, FindActivity::class.java)
+            startActivity(displayIntent)
+        }
+
+        val btnMedia = findViewById<Button>(R.id.btn_media_am)
+        btnMedia.setOnClickListener {
+            val displayIntent = Intent(this, MedialibraryActivity::class.java)
+            startActivity(displayIntent)
+        }
+
+        val btnSettings = findViewById<Button>(R.id.btn_settings_am)
+        btnSettings.setOnClickListener {
+            val displayIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(displayIntent)
+        }
+
     }
 }
